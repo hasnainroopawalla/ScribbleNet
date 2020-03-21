7e1d6342c118dcaa0abe514ebaa59326bc73d8ef
Hasnain Roopawalla <hasnain.roopawalla@gmail.com>
2020-03-21 23:47:29 +0530

    images

--- flask/run.py
+++ flask/run.py
@@ -23,4 +23,4 @@ def predictedclasses():
     return str(objs+pred_accuracy)
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', port=5000, debug=True)
+    app.run(host= '0.0.0.0', port=5000, debug=False)
