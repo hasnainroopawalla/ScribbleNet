fc0715324a2faa7e7d635526a17a6b65d0637963
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2021-12-31 11:57:11 +0100

    Cleaned up server file

--- scribblenet/server.py
+++ scribblenet/server.py
@@ -1,9 +1,3 @@
-import argparse
-import base64
-import os
-import time
-import uuid
-
 from flask import Flask, render_template, request, jsonify
 
 from scribblenet.ml.prediction import predict
@@ -19,17 +13,7 @@ def firstpage():
 @app.route("/predict", methods=["POST"])
 def doodle_predict():
     image_string = request.form["image_string"]
-    return jsonify(predict(image_string))
-
-
-# @app.route("/doodlepredict", methods=["GET", "POST"])
-# def predictedclasses():
-
-#     imgstring = request.form.get("data")
-#     img = data_uri_to_cv2_img(imgstring)
-#     cv2.imwrite(input_img_path, img)
-#     objs, pred_accuracy = predict.predict_doodle(input_img_path)
-#     return str(objs + pred_accuracy)
+    return jsonify({"prediction": predict(image_string)})
 
 
 def main():
