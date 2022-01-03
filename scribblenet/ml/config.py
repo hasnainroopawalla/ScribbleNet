a2f4e26e3f3f9e57f3c7dcf7aa2b6bd6e68d7fa7
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-01-03 19:54:45 +0100

    Cleaned up MLConfig

--- scribblenet/ml/config.py
+++ scribblenet/ml/config.py
@@ -3,6 +3,17 @@ import os
 
 
 class MLConfig:
+    """A class where all the Machine Learning specific configuration lives.
+
+    Attributes:
+        trained_model_path (str): Local path of the trained model requied for predictions.
+        image_dims (Tuple[int, int]): Dimensions of each image used for resizing.
+        classes_path_100 (str): Local path to the 100_classes.txt class names file.
+        classes_path_all (str): Local path to the all_classes.txt class names file.
+        dataset_url (str): URL of the Quickdraw dataset required to fetch the dataset for training.
+        dataset_path (str): Local path where the downloaded dataset will be stored.
+    """
+
     trained_model_path: str = os.path.join(
         os.path.dirname(__file__), "models/trained_model.h5"
     )
@@ -15,76 +26,3 @@ class MLConfig:
     )
     dataset_url: str = "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/"
     dataset_path: str = os.path.join(os.path.dirname(__file__), "data/")
-    classes: List[str] = [
-        "Smiley Face",
-        "Alarm Clock",
-        "Donut",
-        "Butterfly",
-        "Hat",
-        "Wristwatch",
-        "Paper Clip",
-        "Rainbow",
-        "Microphone",
-        "Screwdriver",
-        "Chair",
-        "Spoon",
-        "Snake",
-        "Cloud",
-        "Scissors",
-        "Eye",
-        "Bridge",
-        "Suitcase",
-        "Light Bulb",
-        "Mushroom",
-        "Book",
-        "Dumbbell",
-        "Flower",
-        "Laptop",
-        "Ice Cream",
-        "Lightning",
-        "Sword",
-        "Tree",
-        "Headphones",
-        "Moon",
-        "Baseball Bat",
-        "Ceiling Fan",
-        "Sun",
-        "Bed",
-        "Cup",
-        "Table",
-        "Hammer",
-        "Pants",
-        "Lollipop",
-        "Ladder",
-        "Tennis Racquet",
-        "Cat",
-        "Car",
-        "Fan",
-        "T-shirt",
-        "Umbrella",
-        "Bench",
-        "Airplane",
-        "Envelope",
-        "Coffee Cup",
-        "Hot Dog",
-        "Pizza",
-        "Cell Phone",
-        "Radio",
-        "Baseball",
-        "Camera",
-        "Bird",
-        "Star",
-        "Spider",
-        "Pencil",
-        "Key",
-        "Mountain",
-        "Circle",
-        "Cookie",
-        "Candle",
-        "Sock",
-        "Triangle",
-        "Basketball",
-        "Knife",
-        "Apple",
-        "Clock",
-    ]
